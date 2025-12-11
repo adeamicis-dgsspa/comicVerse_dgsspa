3522d81f385278dc9cd2354cb94f6c7a8a182d90
adeamicis-dgsspa <alessio.deamicis@dgsspa.com>
2025-12-11 12:38:50 +0100

    aggiunta dto Autore e Genere

---
+++ comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/dto/AutoreDTO.java
@@ -0,0 +1,50 @@
+package com.dgsspa.comicverse.dto;
+
+import jakarta.validation.constraints.*;
+
+public class FumettoDTO {
+
+    @NotBlank(message = "Campo Obbligatorio")
+    @Pattern(regexp = "^[\\p{L}'\\s]+$", message = "Il nome dell'autore può contenere solo lettere, spazi e apostrofi")
+    private String nome_cognome;
+
+    @NotBlank(message = "Campo Obbligatorio")
+    private String biografia;
+
+    @NotNull(message = "Campo vivo Obbligatorio")
+    private boolean vivo;
+
+    public AutoreDTO() {
+    }
+
+    public AutoreDTO(String nome_cognome, String biografia, boolean vivo) {
+        this.nome_cognome=nome_cognome;
+        this.biografia=biografia;
+        this.vivo=vivo;
+    }
+
+    public String getNome_cognome() {
+        return nome_cognome;
+    }
+
+    public void setNome_cognome(String nome_cognome) {
+        this.nome_cognome = nome_cognome;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+}
---
+++ comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/dto/GenereDTO.java
@@ -0,0 +1,26 @@
+package com.dgsspa.comicverse.dto;
+
+import jakarta.validation.constraints.*;
+import java.time.LocalDateTime;
+
+public class FumettoDTO {
+    @NotBlank(message = "Campo Obbligatorio")
+    @Pattern(regexp = "^[\\p{L}]+$", message = "Il nome dell'editore può contenere solo lettere")
+
+    private String nome;
+
+    public GenereDTO() {
+    }
+
+    public GenereDTO(String nome) {
+        this.titolo = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
