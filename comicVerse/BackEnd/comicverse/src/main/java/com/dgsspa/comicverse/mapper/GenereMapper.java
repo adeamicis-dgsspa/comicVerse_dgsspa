1fe38dbb59dc93fca0bd0c543105e6887ba03bf9
adeamicis-dgsspa <alessio.deamicis@dgsspa.com>
2026-04-09 12:53:16 +0200

    sistemato import in fumettoMapper e implementato GenereMapper

--- comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/mapper/GenereMapper.java
+++ comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/mapper/GenereMapper.java
@@ -1,11 +1,16 @@
-/*package com.dgsspa.comicverse.mapper;
+package com.dgsspa.comicverse.mapper;
 
-import  com.dgsspa.comicverse.dto.GenereDTO;
+import com.dgsspa.comicverse.dto.GenereDTO;
 import com.dgsspa.comicverse.model.Genere;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {FumettoMapper.class})
 public interface GenereMapper {
     GenereDTO toDTO(Genere genere);
     Genere toEntity(GenereDTO genereDTO);
-}*/
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(GenereDTO dto, @MappingTarget Genere entity);
+}
