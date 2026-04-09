1d61f1c0e8284ed8530aebdc007f2392b1f9d770
adeamicis-dgsspa <alessio.deamicis@dgsspa.com>
2026-04-09 12:04:06 +0200

    modifica delle classi entity fumetto e genere

--- comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/model/Genere.java
+++ comicVerse/BackEnd/comicverse/src/main/java/com/dgsspa/comicverse/model/Genere.java
@@ -1,53 +1,42 @@
-/*package com.dgsspa.comicverse.model;
-
-import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+package com.dgsspa.comicverse.model;
+import javax.persistence.*;
 import java.util.List;
 import java.util.ArrayList;
-
 @Entity
-@Table (name="genere")
+@Table(name = "GENERE")
 public class Genere {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GENERE")
+    @SequenceGenerator(name = "SEQ_GENERE", sequenceName = "S_GENERE", allocationSize = 1)
+    @Column(name = "ID_GENERE")
     private Integer id;
-
     private String nome;
-
-    @ManytoMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
-            name = "genere_fumetto",
-            joinColumns = @JoinColumn(name = "genere_id"),
-            inverseJoinColumns = @JoinColumn(name = "fumetto_id")
+            name = "GENERE_FUMETTO",
+            joinColumns = @JoinColumn(name = "FK_GENERE"),
+            inverseJoinColumns = @JoinColumn(name = "FK_FUMETTO")
     )
-    private List<Fumetto> fumetti;
-
-    public Genere() {
-    }
-/*----------------------------------------------------------------------------------------*/
-/*
+    private List<Fumetto> fumetti = new ArrayList<>();
+    public Genere() {}
+    /*----------------------------------------------------------------------------------------*/
     public Integer getId() {
         return id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public List<Fumetto> getFumetti() {
         return fumetti;
     }
-
-/*----------------------------------------------------------------------------------------*/
-/*
+    /*----------------------------------------------------------------------------------------*/
     public void setId(Integer id) {
         this.id = id;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public void setFumetti(List<Fumetto> fumetti) {
         this.fumetti = fumetti;
     }
-}*/
+}
